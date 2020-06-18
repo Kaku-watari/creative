ddca40387cbd3e192506f5ed02331b6f5997290a
Kaku-Ishiwatari <44764936+Kaku-Ishiwatari@users.noreply.github.com>
2020-06-18 23:09:50 +0900

    change email address

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-creative/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'HARRY Co.,Ltd.',
-  subHeading: 'We Are Web Developers',
+  heading: 'Kaku Web Design',
+  subHeading: 'I am a Web Developer',
 
   // social
   socialLinks: [
@@ -34,7 +34,7 @@ module.exports = {
       url: 'https://creative-samplesite.netlify.com/',
     },
   ],
-  email: 'info@bhb-harry.co.jp',
-  phone: 'Please contact as by email for the first time.',
-  address: '4F Hanzomon KD Bld. 1-8-9 Hirakawa-cho, Chuo-ku, Tokyo, Japan Zip:102-0093',
+  email: '0825kaku@gmail.com',
+  phone: 'Please contact by email for the first time.',
+  address: 'Kanagawa, Japan',
 };
